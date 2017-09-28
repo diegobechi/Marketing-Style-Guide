d9b64733ae04e8eb9714ce652d56efae31b567d9
Diego <diegobechi@gmail.com>
2017-09-28 13:36:02 +1000

    new layout system

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php include('templates/static/header.php'); ?>
 
-		Here we should make a description of the use for the page.
+	<h1>Welcome to the ReCharge Style Guide</h1>
+	<p>This style guide was created to have all the styles and design specifications in one place.</p>
+	<p>Basically here we have a set of standars CSS styles applies all accross the website to keep the 
+	   consistence of it and also to be used in external publications.</p>
 
+	<br>
+	<p>If you have any questions or suggestions please email us at support@rechargepaymetns.com</p>
 
 <?php include('templates/static/footer.php'); ?>
--- templates/static/header.php
+++ templates/static/header.php
@@ -6,10 +6,10 @@
 	<body class="rc_sg">
 		<?php include('navbar.php'); ?>
 		<div class="rc_sg__container">
-			<div class="container">
-				<div class="rc_sg__container__menu grid__xs__3">
+			<div class="container flex__layout">
+				<div class="rc_sg__container__menu flex__sm__12 flex__md__3 flex__lg__3">
 					<?php include('main_menu.php'); ?>
 				</div>
 				
-				<div class="rc_sg__pagedetails grid__xs__9">
+				<div class="rc_sg__pagedetails flex__sm__12 flex__md__9 flex__lg__9">
 				
