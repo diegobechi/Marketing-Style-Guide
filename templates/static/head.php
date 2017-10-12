7cf23290225f7ed16e72d1f44cef5c9f7a327996
Diego <diegobechi@gmail.com>
2017-10-13 09:28:40 +1100

    added google font and changes in typography page

--- templates/static/head.php
+++ templates/static/head.php
@@ -5,6 +5,10 @@
 
 <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
 <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+
+<!--Raleway - Google Fonts-->
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,400,500,600,700,900&amp;subset=latin-ext" rel="stylesheet">
+
 <!-- Added jQuery -->
 <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
 
