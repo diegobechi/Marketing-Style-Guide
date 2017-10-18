9320c0e68e264ac31c7a2cfaf586470db709809a
Diego <diegobechi@gmail.com>
2017-10-18 14:06:19 +1100

    finshed typography styles

--- typography.php
+++ typography.php
@@ -5,35 +5,35 @@
 		<h2>Typography</h2>
 		<p>With ultimate flexibility, we use Raleway exclusively throughout the marketing website.</p>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section typography_section">
 		<h2 class="h1">This is an H1</h2>
 		<p class="rc_caption">font-family: Ralewaysemibold; font-size: 42px; line-height: 48px; margin-bottom: 36px;</p>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section typography_section">
 		<h2>This is an H2</h2>
 		<p class="rc_caption">font-family: Ralewaysemibold; font-size: 28px; line-height: 36px; letter-spacing: -.25; margin-bottom: 36px;</p>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section typography_section">
 		<h3>This is an H3</h3>
 		<p class="rc_caption">font-family: Ralewaysemibold; font-size: 24px; line-height: 32px; letter-spacing: -.25; margin-bottom: 36px;</p>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section typography_section">
 		<h4>This is an H4</h4>
 		<p class="rc_caption">font-family: Ralewaysemibold; font-size: 20px; line-height: 24px; margin-bottom: 24px;</p>
 	</div>
-	<div class="rc_sg_section">
-		<p>This is a body copy</p>
+	<div class="rc_sg_section typography_section">
+		<p class="body_text">This is a body copy</p>
 		<p class="rc_caption">font-family: Ralewayregular; font-size: 16px; line-height: 25px;</p>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section typography_section">
 		<p class="rc_small_copy">This is a small body copy</p>
 		<p class="rc_caption">font-family: Ralewayregular; font-size: 14px; line-height: 20px;</p>
 	</div>
-	<div class="rc_sg_section">
-		<p class="rc_caption">This is a caption</p>
+	<div class="rc_sg_section typography_section">
+		<p class="rc_caption example_caption">This is a caption</p>
 		<p class="rc_caption">font-family: Ralewayregular; font-size: 12px; line-height: 18px; letter-sapcing: 2; text-transform: uppercase; color: #424242; opacity: .5;</p>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section typography_section">
 		<div class="rc_sg_section typography_example">
 			<div class="typography_example_item light_example">
 				<div class="circle"></div>	
