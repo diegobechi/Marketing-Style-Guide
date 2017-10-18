208be5f6287d801a19402616256e4027ccd31b7a
Diego <diegobechi@gmail.com>
2017-10-18 15:19:54 +1100

    finished shadows styles page

--- shadows.php
+++ shadows.php
@@ -7,49 +7,38 @@
 	</div>
 	<div class="rc_sg_section">
 		<div class="rc_sg_shadow_example">
-			<div>
-				<div class="rc_sg_shadow_example_item rc_shadow_level1"><label>Level 1</label></div>				
-			</div>
-			<div>
-				<div class="rc_sg_shadow_example_item rc_shadow_level2"><label>Level 2</label></div>	
-			</div>
-			<div>
-				<div class="rc_sg_shadow_example_item rc_shadow_level3"><label>Level 3</label></div>	
-			</div>
-			<div>
-				<div class="rc_sg_shadow_example_item rc_shadow_level4"><label>Level 4</label></div>	
-			</div>
-			<div>
-				<div class="rc_sg_shadow_example_item rc_shadow_level5"><label>Level 5</label></div>	
-			</div>
+			<div class="rc_sg_shadow_example_item rc_shadow_level1"><label>Level 1</label></div>
+			<div class="rc_sg_shadow_example_item rc_shadow_level2"><label>Level 2</label></div>
+			<div class="rc_sg_shadow_example_item rc_shadow_level3"><label>Level 3</label></div>
+			<div class="rc_sg_shadow_example_item rc_shadow_level4"><label>Level 4</label></div>	
+			<div class="rc_sg_shadow_example_item rc_shadow_level5"><label>Level 5</label></div>	
 		</div>
-		
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section shadow_section">
 		<h3>Level 1</h3>
 		<label class="rc_caption">-webkit-box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.1);</label><br>
 		<label class="rc_caption">-moz-box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.1);</label><br>
 		<label class="rc_caption">box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.1);</label><br>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section shadow_section">
 		<h3>Level 2</h3>
 		<label class="rc_caption">-webkit-box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.1);</label><br>
 		<label class="rc_caption">-moz-box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.1);</label><br>
 		<label class="rc_caption">box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.1);</label><br>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section shadow_section">
 		<h3>Level 3</h3>
 		<label class="rc_caption">-webkit-box-shadow: 0px 6px 12px 0px rgba(0,0,0,0.1);</label><br>
 		<label class="rc_caption">-moz-box-shadow: 0px 6px 12px 0px rgba(0,0,0,0.1);</label><br>
 		<label class="rc_caption">box-shadow: 0px 6px 12px 0px rgba(0,0,0,0.1);</label><br>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section shadow_section">
 		<h3>Level 4</h3>
 		<label class="rc_caption">-webkit-box-shadow: 0px 10px 30px 0px rgba(0,0,0,0.1);</label><br>
 		<label class="rc_caption">-moz-box-shadow: 0px 10px 30px 0px rgba(0,0,0,0.1);</label><br>
 		<label class="rc_caption">box-shadow: 0px 10px 30px 0px rgba(0,0,0,0.1);</label><br>
 	</div>
-	<div class="rc_sg_section">
+	<div class="rc_sg_section shadow_section">
 		<h3>Level 5</h3>
 		<label class="rc_caption">-webkit-box-shadow: 0px 20px 40px 0px rgba(0,0,0,0.1);</label><br>
 		<label class="rc_caption">-moz-box-shadow: 0px 20px 40px 0px rgba(0,0,0,0.1);</label><br>
