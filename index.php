f0da7e38bb403bdd937f04e376bb0e442fe37ce8
Corey Capetillo <corey.capetillo@gmail.com>
2017-10-25 13:50:28 -0700

    Typo in email. Sentences on 1 line

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 
 	<h1>Welcome to the ReCharge Style Guide</h1>
 	<p>This style guide was created to have all the styles and design specifications in one place.</p>
-	<p>Basically here we have a set of standars CSS styles applies all accross the website to keep the 
-	   consistence of it and also to be used in external publications.</p>
+	<p>Basically here we have a set of standars CSS styles applies all accross the website to keep the consistence of it and also to be used in external publications.</p>
 
 	<br>
-	<p>If you have any questions or suggestions please email us at support@rechargepaymetns.com</p>
+	<p>If you have any questions or suggestions please email us at support@rechargepayments.com</p>
 
 <?php include('templates/static/footer.php'); ?>
