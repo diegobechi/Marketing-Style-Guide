872df3370190fea425e6141f1541ccaf12df4485
Corey Capetillo <corey.capetillo@gmail.com>
2017-11-03 11:14:13 -0700

    Add grid system

--- grid-system.php
+++ grid-system.php
@@ -3,7 +3,25 @@
 <div>
 	<div class="sg_page_header">
 		<h2>Grid system</h2>
-		<p>The marketing website grid is 12 columns totalling 1170px wide. The grid has 60px columns and 28px wide gutters. ReCharge uses a 6px baseline grid for spacing.</p>
+		<p>The marketing website grid is 12 columns totalling 1170px at maximum width. The grid has 8.33% columns with 22px wide gutters. ReCharge uses a 6px baseline grid for spacing.</p>
+	</div>
+	<div class="sg_section sg_grids_section">
+		<div class="rc_layout__container">
+			<div class="rc_layout">
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+				<div class="rc_layout__sm__1"></div>
+			</div>
+		</div>
 	</div>
 </div>
 
