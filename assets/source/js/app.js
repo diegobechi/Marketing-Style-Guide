50469620c946ff102d566d658b1c3ee71935a272
Corey Capetillo <corey.capetillo@gmail.com>
2017-11-09 15:06:34 -0800

    Rename some JS files so you can tell they're includes

--- assets/source/js/app.js
+++ assets/source/js/app.js
@@ -1,5 +1,5 @@
-//@prepros-append recharge-app.js
-//@prepros-append selects.js
-//@prepros-append countUp.js
-//@prepros-append rc_core.js
-//@prepros-append rc_validation.js
+//@prepros-append _recharge-app.js
+//@prepros-append _selects.js
+//@prepros-append _countUp.js
+//@prepros-append _rc_core.js
+//@prepros-append _rc_validation.js
