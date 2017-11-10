dbbef3f04521e06e7e8c1e4fad82b201f512d1af
Diego <diegobechi@gmail.com>
2017-11-10 17:20:57 +1100

    added assets and worked on media kit new design

--- homepage/media-kit.php
+++ homepage/media-kit.php
@@ -7,62 +7,61 @@
     $page_active = 'media-kit';
 ?>
 <?php include('includes/header.php'); ?>
-
-<div class="mediakit__block">
-    <div class="logos text--center">
-        <h1 class="logos__title">LOGOS &amp; RESOURCES</h1>
-        <p class="logos__description">These are official ReCharge graphics that you can include on your webpage, in your mobile application, or in printed material.</p>
-        <div class="layout__container logos__grid">
-            <div class="layout">
-                <div class="layout__full text--left">
-                    <div class="logos__grid__description">
-                        <p>Recharge full and mark only logos</p>
-                        <p class="logos__grid__description__tiny">The white logos are designed for dark backgrounds.</p>
-                    </div>
-                </div>
-                <div class="grid__sm__6">
-                    <div class="layout">
-                        <div class="layout__full">
-                            <div class="logos__grid__item logos__grid__item--full logos__grid__item--white">
+<div class="rc_section__container">
+    <div class="rc_section--media-kit rc_text--center">
+        <div class="media-kit__header">
+            <div class="media-kit__header__title">
+                <h1>Logos &amp; resources</h1>
+            </div>
+            <div class="media-kit__header__description">
+                <p>These are official ReCharge graphics that you can include on your webpage, in your mobile application, or in printed material.</p>
+            </div>
+            <div class="logos__download">
+                <button id="download-link" class="rc_button rc_button_large rc_button_primary logos__download__link" href="/downloads/ReCharge-Logos.zip" download>Download assets</a>
+            </div>
+        </div>
+        <div class="media-kit__container">
+            <div class="rc_layout">
+                <div class="rc_layout__sm__6">
+                    <div class="rc_layout">
+                        <div class="rc_layout__full">
+                            <div class="logos__rc_layout__item logos__rc_layout__item--full logos__rc_layout__item--white">
                                 <img src="/assets/images/media-kit/RCP-full-logo-color.svg">
                             </div>
                         </div>
-                        <div class="grid__xs__6">
-                            <div class="logos__grid__item logos__grid__item--white">
-                                <img class="logos__grid__item__mark" src="/assets/images/media-kit/RCP_mark_blue.svg">
+                        <div class="rc_layout__xs__6">
+                            <div class="logos__rc_layout__item logos__rc_layout__item--white">
+                                <img class="logos__rc_layout__item__mark" src="/assets/images/media-kit/RCP_mark_blue.svg">
                             </div>
                         </div>
-                        <div class="grid__xs__6">
-                            <div class="logos__grid__item logos__grid__item--white">
-                                <img class="logos__grid__item__mark" src="/assets/images/media-kit/RCP_mark_grey.svg">
+                        <div class="rc_layout__xs__6">
+                            <div class="logos__rc_layout__item logos__rc_layout__item--white">
+                                <img class="logos__rc_layout__item__mark" src="/assets/images/media-kit/RCP_mark_grey.svg">
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="grid__sm__6">
-                    <div class="layout">
-                        <div class="layout__full">
-                            <div class="logos__grid__item logos__grid__item--full logos__grid__item--black">
+                <div class="rc_layout__sm__6">
+                    <div class="rc_layout">
+                        <div class="rc_layout__full">
+                            <div class="logos__rc_layout__item logos__rc_layout__item--full logos__rc_layout__item--black">
                                 <img src="/assets/images/media-kit/RCP-full-logo-white.svg">
                             </div>
                         </div>
-                        <div class="grid__xs__6">
-                            <div class="logos__grid__item logos__grid__item--white">
-                                <img class="logos__grid__item__mark" src="/assets/images/media-kit/RCP_mark_black.svg">
+                        <div class="rc_layout__xs__6">
+                            <div class="logos__rc_layout__item logos__rc_layout__item--white">
+                                <img class="logos__rc_layout__item__mark" src="/assets/images/media-kit/RCP_mark_black.svg">
                             </div>
                         </div>
-                        <div class="grid__xs__6">
-                            <div class="logos__grid__item logos__grid__item--black">
-                                <img class="logos__grid__item__mark" src="/assets/images/media-kit/RCP_mark_white.svg">
+                        <div class="rc_layout__xs__6">
+                            <div class="logos__rc_layout__item logos__rc_layout__item--black">
+                                <img class="logos__rc_layout__item__mark" src="/assets/images/media-kit/RCP_mark_white.svg">
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>            
         </div>
-        <div class="logos__download">
-            <a id="download-link" class="logos__download__link" href="/downloads/ReCharge-Logos.zip" download>Download logo kit</a>
-            <p class="logos__download__file-ext">EPS, SVG, PNG</p>
         </div>
     </div>
 </div>
