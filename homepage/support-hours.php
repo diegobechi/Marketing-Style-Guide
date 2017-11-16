d04f8cb73d024738caf62b5a389afc2db7e19194
Diego <diegobechi@gmail.com>
2017-11-16 19:33:31 +1100

    added the support hours page

---
+++ homepage/support-hours.php
@@ -0,0 +1,56 @@
+
+<?php
+$page_title = 'Support Hours';
+$page_description = 'We will always aim to respond to your messages within 12 hours, if not sooner! ';
+$page_keywords = 'Support Hours';
+$page_class = 'experts';
+$page_active = 'support-hours';
+?>
+<?php include('includes/header.php'); ?>
+
+<div class="rc_section__container">
+	<div class="rc_section--support-hours rc_text--center">
+		<div class="rc_text_banner__header">
+			<div class="rc_text_banner__header__title">
+				<h1>ReCharge customer success</h1>
+			</div>
+			<div class="rc_text_banner__header__description">
+				<p>We’ll provide the best service in the quickest manner possible.</p>
+			</div>
+		</div>
+	</div>
+	<div class="rc_text_banner__container">
+		<div class="rc_layout__container rc_support-hours">
+			<div class="rc_layout">
+				<div>
+					<h2>Our promise to you</h2>
+					<p>ReCharge will provide the best service in the quickest manner possible.</p>
+				</div>
+				<div>
+					<h2>Support Hours</h2>
+					<p>We will always aim to respond to your messages within 12 hours, if not sooner! Our office hours are Monday through Friday, 7am to 5pm Pacific. If your email is sent outside those hours, you’ll receive a response when our team gets back into the office.</p>
+				</div>
+				<div>
+					<h2>Why We Stick to Emails</h2>
+					<p>Our belief is that email is the best format in which to support any technical product because it allows you to communicate exactly what is going wrong, and for us to take the time to fully troubleshoot your issue without keeping you from doing what you do best: managing your business.</p>
+				</div>
+				<div>
+					<h2>Help Us Help You</h2>
+					<p>
+						We want to help, and sending details will save everyone time! Help us help you by including all of the information you can think of in your first message to us. For example: 
+						<ul>
+							<li>If you are having issues with orders, include the name and email of the customers affected</li>
+							<li>If there is a front-end issue, send a screenshot and URL</li>
+							<li>If there is a problem with an order flow, include as many details as possible</li>
+						</ul>
+						We believe in doing what is right for our users. We will work tirelessly to make sure that if your issue is with us, it is resolved; and if it is not we will work with you to point you in the right direction. In return we ask only for your patience while we work together to find a solution. </p>
+					</div>
+					<div>
+						<h2>Resources</h2>
+						<p>In the meantime, we encourage you to visit our ever-growing library of <a href="http://support.rechargepayments.com/" target="_blank">FAQs</a>, which will not only help you with any issues which you might experience, but will also help you maximize the ways in which you can leverage ReCharge to grow your business.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include('includes/footer.php'); ?>
