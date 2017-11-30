8986449f6aa86892381c1b4990166e8c198708e8
Diego <diegobechi@gmail.com>
2017-11-30 15:21:21 +1100

    added informative pages and their assets

---
+++ homepage/privacy-policy.php
@@ -0,0 +1,224 @@
+<?php
+	$page_title = 'Privacy Policy';
+	$page_description = 'This Privacy Policy was written to help you better understand how we collect, use and store your information.';
+	$page_keywords = 'Privacy Policy, Privacy, Security, Trust';
+	$page_class = 'privacy_policy';
+	$page_active = 'privacy-policy';
+?>
+<?php include('templates/static/header.php'); ?>
+
+<div class="rc_section__container">
+	<div class="rc_section--support-hours rc_text--center">
+		<div class="rc_text_banner__header">
+			<div class="rc_text_banner__header__title">
+				<h1>Privacy policy</h1>
+			</div>
+			<div class="rc_text_banner__header__description">
+				<p></p>
+			</div>
+		</div>
+	</div>
+	<div class="rc_text_banner__container">
+		<div class="rc_layout__container rc_informative-page">
+			<div class="rc_layout__full">
+				<div class="rc_informative-page__block">
+					<h3>Section 1 - Information from our Merchants</h3>
+					<p>When you install the ReCharge Payments app, we receive information about you from Shopify:</p>
+					<ul>
+						<li>
+							We collect your name, email, business address, phone, and timezone.
+							<ul>
+								<li>We require this information to provide you with our service, for example to be able to contact you, properly show dates on your checkout, and correctly format invoice information.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							We collect data about the ReCharge hosted websites you visit, as well as information about how and when you visit, your network information (such as the IP address).
+							<ul>
+								<li>We need this information to give you access to and improve our Services.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							We collect personal information on your customers that you share with us or customers provide while shopping or during checkout.
+							<ul>
+								<li>We use this information to provide you with our Services and so that you can process orders and better serve your customers.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>We will also use Personal Information in other cases where you have given us your express permission.</li>
+					</ul>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 2 - Information from our Merchants' Customers</h3>
+					<p>When you purchase something from a store we host the checkout for, as part of the buying and selling process, we collect the personal information your customers provide us.</p>
+					<ul>
+						<li>
+							We collect the customer's name, email, shipping, and billing address.
+							<ul>
+								<li>We require this information in order to provide you and your customer with our e-commerce service.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							We collect the customer's credit card or paypal information.
+							<ul>
+								<li>We require this information in order to bill your customer, and fund your business.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							When customers browse your checkout and create orders, we collect information about their computer and network traffic.
+							<ul>
+								<li>We use this information for security and to provide you and your customer with our services.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>We will also use Personal Information in other cases where you have given us your express permission.</li>
+					</ul>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 3 - Information from our Partners</h3>
+					<p>Partners are individuals or businesses who have partnered with ReCharge to provide an integration or a service to the merchants who use the ReCharge platform. When you register as a ReCharge Partner we collect some information about your business in order to accurately present information about your business to our merchants.</p>
+					<ul>
+						<li>
+							We collect a Partner's name, address, phone number, and email.
+							<ul>
+								<li>We require this information in order to work with you, confirm your identity, and contact you.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							We collect data about the ReCharge pages you visit and how you use your account. This includes network and computer information, such as your IP address and Browser.
+							<ul>
+								<li>We use this information to give you access and improve our services.</li></ul>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							We collect Personal Information about your customers that you share with us or that they provide to us directly
+							<ul>
+								<li>We use this information to work with you and to provide our Services to your customers.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>We will also use Personal Information in other cases where you have given us express permission.</li>
+					</ul>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 4 - Consent</h3>
+					<h5>How do you get my consent?</h5>
+					<p>When you provide us with personal information to complete a transaction, verify your credit card, place an order, arrange for a delivery or return a purchase, we imply that you consent to our collecting it and using it for that specific reason only.</p>
+					<p>If we ask for your personal information for a secondary reason, like marketing, we will either ask you directly for your expressed consent, or provide you with an opportunity to say no.</p>
+
+					<h5>How do I access my personal information?</h5>
+					<p>You retain all rights to your Personal Information and can access, amend, and correct it anytime. Both Merchants and their customers have full access to update or delete their Personal Information from the Merchant admin access or the Customer Portal. If you are unable to change any Personal Information, please contact us to make the changes. It is important to remember if you delete or limit the use of your Personal Information, the Service we provide may not function properly.</p>
+
+					<h5>How do I withdraw my consent?</h5>
+					<p>If after you opt-in, you change your mind, you may withdraw your consent for us to contact you, for the continued collection, use or disclosure of your information, at anytime, by contacting us at <?php echo hide_email('support@rechargeapps.com'); ?> or mailing us at:</p>
+					<br>
+					<address>
+						<strong>Bootstrap Heroes LLC</strong>
+						<p>
+							3030 Nebraska Avenue<br>
+							Los Angeles California US 90404
+						</p>
+					</address>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 5 - Disclosure</h3>
+					<p>We may disclose your personal information if we are required by law to do so or if you violate our Terms of Service.</p>
+					<p>If you believe your Personal Information has been used in a manner not consistent with the policies outlined, please contact us using the information provided below. If your complaint remains unresolved, you may also contact the International Centre for Dispute Resolution®, the international division of the American Arbitration Association® (ICDR/AAA). This organization provides independent dispute resolution services, at no charge to you. ICDR/AAA can be contacted at <a class="rc_link" href="http://go.adr.org/privacyshield.html" target="_blank">http://go.adr.org/privacyshield.html</a></p>
+					<p>If, after attempting to resolve a dispute through ICDR/AAA, you feel that your concerns about the use of your Personal Information have not been resolved, you may seek resolution of the issue through binding arbitration. For more information about the binding arbitration process, please visit <a class="rc_link" href="http://www.privacyshield.gov" target="_blank">http://www.privacyshield.gov</a>.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 6 - Shopify</h3>
+					<p>Our store is hosted on Shopify Inc. They provide us with the online e-commerce platform that allows us to sell our products and services to you.</p>
+					<p>Your data is stored through Shopify's data storage, databases and the general Shopify application. They store your data on a secure server behind a firewall.</p>
+					<h5>Payment</h5>
+					<p>If you choose a direct payment gateway to complete your purchase, then Shopify stores your credit card data. It is encrypted through the Payment Card Industry Data Security Standard (PCI-DSS). Your purchase transaction data is stored only as long as is necessary to complete your purchase transaction. After that is complete, your purchase transaction information is deleted.</p>
+					<p>All direct payment gateways adhere to the standards set by PCI-DSS as managed by the PCI Security Standards Council, which is a joint effort of brands like Visa, MasterCard, American Express and Discover.</p>
+					<p>PCI-DSS requirements help ensure the secure handling of credit card information by our store and its service providers.</p>
+					<p>For more insight, you may also want to read Shopify's <a class="rc_link" href="https://www.shopify.com/legal/terms" target="_blank">Terms of Service</a> or <a class="rc_link" href="https://www.shopify.com/legal/privacy" target="_blank">Privacy Statement</a>.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 7 - Information shared with Third-Party Services</h3>
+					<p>The third-party providers used by us will only collect, use and disclose your information to the extent necessary to allow them to perform the services they provide to you and to us.</p>
+					<ul>
+						<li>
+							We may share customer name, email, phone, shipping and billing address information information of Merchant's customers with Shopify.
+							<ul>
+								<li>We provide this information to Shopify in order to provide our merchants with our service.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							We may share credit card and billing address information of Merchant's customers with payment gateways.
+							<ul>
+								<li>We provide this information to payment gateways in order to provide our merchants with our service.</li>
+							</ul>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							We may share shipping address information information of Merchant's customers with address validation and fullfillment services.
+							<ul>
+								<li>We provide this information to fullfillment services in order to provide our merchants with our service.</li>
+							</ul>
+						</li>
+					</ul>
+					<p>Certain third-party service providers, such as payment gateways and other payment transaction processors, have their own privacy policies in respect to the information we are required to provide to them for your purchase-related transactions.</p>
+					<p>For these providers, we recommend that you read their privacy policies so you can understand the manner in which your personal information will be handled by these providers.</p>
+					<p>ReCharge is responsible for all transfers of Personal Information to third parties in accordance with the EU-U.S. Privacy Shield Principles. Any Personal Information transferred to a third party for data processing is processed in accordance with our instructions, and is subject, by law, to a comparable level of protection as that provided by ReCharge.</p> 
+					<p>ReCharge will always ask for your consent before sharing your Personal Information with a Third Party for purposes other than than those described in this section.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 8 - Security</h3>
+					<p>To protect your personal information, we take reasonable precautions and follow industry best practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed.</p>
+					<p>If you provide us with your credit card information, the information is encrypted using secure socket layer technology (SSL) and stored with a AES-256 encryption.</p>
+					<p>No method of transmission over the Internet, or method of electronic storage, is 100% secure. Therefore, we cannot guarantee the absolute security of your Personal Information.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 9 - Cookies</h3>
+					<p>What is a cookie? A cookie is a small amount of data, which may include a unique identifier. Cookies are sent to your browser from a website and stored on your device. We assign a different cookie to each device that accesses our website.</p>
+					<p>We use cookies to recognize your device and provide you with our service and a personalized experience.</p>
+					<p>Our third-party integrations may use cookies to provide you with their service, or to serve targeted ads. You can opt out of targeted ads served via specific third party vendors by visiting the Digital Advertising Alliance’s <a class="rc_link" href="http://optout.aboutads.info/#!/" target="_blank">Opt-Out page</a>.</p>
+					<p>We may also use web beacons, tracking technology and other automated tracking methods on our websites, in communications with you, and in our products and services, to measure performance and engagement.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 10 - Age of Consent</h3>
+					<p>By using this site, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Section 11 - Changes to This Privacy Policy</h3>
+					<p>We reserve the right to modify this privacy policy at any time, so please review it frequently. Changes and clarifications will take effect immediately upon their posting on the website. If we make material changes to this policy, we will notify you here that it has been updated, so that you are aware of what information we collect, how we use it, and under what circumstances, if any, we use and/or disclose it.</p>
+					<p>If our store is acquired or merged with another company, your information may be transferred to the new owners so that we may continue to sell products to you. If this occurs, we will notify all of our merchants and customers and post on our home page.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Questions and Contact Information</h3>
+					<p>If you would like to: access, correct, amend or delete any personal information we have about you, register a complaint, or simply want more information contact our <strong>Privacy Compliance Officer</strong> at <?php echo hide_email('support@rechargeapps.com'); ?> or by mail at:</p>
+					<br>
+					<address>
+						<strong>Bootstrap Heroes LLC</strong>
+						<p>
+							3030 Nebraska Avenue<br>
+							Los Angeles California US 90404
+						</p>
+					</address>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include('templates/static/footer.php'); ?>
---
+++ homepage/support-hours.php
@@ -0,0 +1,57 @@
+
+<?php
+$page_title = 'Support Hours';
+$page_description = 'We will always aim to respond to your messages within 12 hours, if not sooner! ';
+$page_keywords = 'Support Hours';
+$page_class = 'experts';
+$page_active = 'support-hours';
+?>
+<?php include('templates/static/header.php'); ?>
+
+<div class="rc_section__container">
+	<div class="rc_section--support-hours rc_text--center">
+		<div class="rc_text_banner__header">
+			<div class="rc_text_banner__header__title">
+				<h1>Customer success</h1>
+			</div>
+			<div class="rc_text_banner__header__description">
+				<p>We’ll provide the best service in the quickest manner possible.</p>
+			</div>
+		</div>
+	</div>
+	<div class="rc_text_banner__container">
+		<div class="rc_layout__container rc_informative-page">
+			<div class="rc_layout__full">
+				<div class="rc_informative-page__block">
+					<h3>Our promise to you</h3>
+					<p>ReCharge will provide the best service in the quickest manner possible.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Support hours</h3>
+					<p>We will always aim to respond to your messages within 12 hours, if not sooner! Our office hours are Monday through Friday, 7am to 5pm Pacific. If your email is sent outside those hours, you’ll receive a response when our team gets back into the office.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Why we stick to emails</h3>
+					<p>Our belief is that email is the best format in which to support any technical product because it allows you to communicate exactly what is going wrong, and for us to take the time to fully troubleshoot your issue without keeping you from doing what you do best: managing your business.</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Help us help you</h3>
+					<p>
+						We want to help, and sending details will save everyone time! Help us help you by including all of the information you can think of in your first message to us. For example: 
+						<ul>
+							<li>If you are having issues with orders, include the name and email of the customers affected</li>
+							<li>If there is a front-end issue, send a screenshot and URL</li>
+							<li>If there is a problem with an order flow, include as many details as possible</li>
+						</ul>
+						We believe in doing what is right for our users. We will work tirelessly to make sure that if your issue is with us, it is resolved; and if it is not we will work with you to point you in the right direction. In return we ask only for your patience while we work together to find a solution. 
+					</p>
+				</div>
+				<div class="rc_informative-page__block">
+					<h3>Resources</h3>
+					<p>In the meantime, we encourage you to visit our ever-growing library of <a href="http://support.rechargepayments.com/" target="_blank">FAQs</a>, which will not only help you with any issues which you might experience, but will also help you maximize the ways in which you can leverage ReCharge to grow your business.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	<?php include('templates/static/footer.php'); ?>
