4124d8201806b3edcbd214d1ce3e19cfb72f276b
Diego <diegobechi@gmail.com>
2017-12-14 21:28:53 +1100

    added the developers page

---
+++ homepage/developers.php
@@ -0,0 +1,252 @@
+<?php
+    // require 'vendor/autoload.php';
+    $page_title = 'Developers';
+    $page_description = 'Explore the possibilities to customize subscriptions for your brand.';
+    $page_keywords = 'Recharge API, API, Shopify, customize, pro';
+    $page_class = 'api';
+    $page_active = 'api';
+    $navbar_button = '<a href="#request-access" class="button button--large button--magenta navbar-button-right pull-right request-access-button" style="display: none;">Request Access</a>';
+?>
+<?php include('templates/static/header.php'); ?>
+
+<div class="background_api"></div>
+
+<div class="rc_section__container">
+    <div class="rc_section rc_section--hero rc_text--center">
+        <div class="rc_content">
+            <h1 class="rc_font--regular">A <span class="rc_color_blue">powerful API</span> to level up yout game</h1>
+            <p>Solve your complex subscription needs</p>
+            <a href="#" class="rc_button rc_button_primary">Request access</a>
+        </div>
+    </div>
+</div>
+
+<div class="rc_section__container">
+    <div class="rc_section rc_section--custom-workflows">
+        <div class="rc_layout__container">
+            <div class="rc_layout">
+                <div class="rc_layout__md__6 rc_content__container">
+                    <div class="rc_content">
+                        <h3>Create custom workflows</h3>
+                        <p>Use the ReCharge API to build the custom solutions you need to grow your business:</p>
+                        <table class="feature-table">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-1.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Automate your email marketing</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-2.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Create free trials of your subscriptions products</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-3.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Cross-sell other subscription products</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-3.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Build custom cancellation flows to reduce churn</strong></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="rc_layout__md__6 rc_media__container">
+                    <img src="/assets/images/developers/custom-workflow.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="rc_section__container">
+    <div class="rc_section rc_section--valuable-insights">
+        <div class="rc_layout__container">
+            <div class="rc_layout">
+                <div class="rc_layout__md__6 rc_media__container">
+                    <div class="revenue_stats">
+                        <div id="revenue_stats__dollars">$740,216</div>
+                        Projected revenue for next 30 days
+                    </div>
+                    <video width="100%" muted loop autoplay poster="/assets/videos/developers.jpg" style="max-width: 462px">
+                        <source src="/assets/videos/developers.webm" type="video/webm">
+                        <source src="/assets/videos/developers.mp4" type="video/mp4">
+                        <source src="/assets/videos/developers.ogg" type="video/ogg">
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+                <div class="rc_layout__md__6 rc_content__container">
+                    <div class="rc_content">
+                        <h3>Collect valuable insights</h3>
+                        <p>Get all the insights you need to grow your customer base – and your business:</p>
+                        <table class="feature-table">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-1.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Get specific data and customized reports</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-2.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Send surveys to collect customer insights</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-3.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Generate real-time analytics</strong></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="rc_section__container">
+    <div class="rc_section rc_section--valuable-insights">
+        <div class="rc_layout__container">
+            <div class="rc_layout">
+                <div class="rc_layout__md__6 rc_content__container">
+                    <div class="rc_content">
+                        <h3>Tailor your customer portal</h3>
+                        <p>Now you can customize how your customers manage their subscriptions and account details:</p>
+                        <table class="feature-table">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-1.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Change the look and feel of your customer portal to match your brand</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-2.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Create a seamless user experience across your entire store</strong></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-3.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Add or remove functionality on the portal</strong></td>
+                                </tr>                                
+                                <tr>
+                                    <td>
+                                        <img src="/assets/images/layout/merchants-icon-3.svg" alt="icon">
+                                    </td>
+                                    <td><strong class="rc_text--small">Develop a custom product page and checkout experience</strong></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="rc_layout__md__6 rc_media__container">
+                    <img src="/assets/images/developers/customer-portal-dr-axe.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="rc_section__container">
+    <div class="rc_section rc_section--overview rc_text--center">
+        <h3>Use ReCharge with other tools</h3>
+        <p>The API allows for more third-party integrations with ReCharge, so all the parts of your business can work together.</p>
+        <div class="rc_layout__container">
+            <div class="rc_layout">
+                <div class="rc_layout__md__3">
+                    <div class="overview_feature">
+                        <img src="/assets/images/layout/overview-1.svg">
+                        <p>Loyalty programs</p>
+                    </div>
+                </div>
+                <div class="rc_layout__md__3">
+                    <div class="overview_feature">
+                        <img src="/assets/images/layout/overview-1.svg">
+                        <p>Marketing automation and metrics</p>
+                    </div>
+                </div>
+                <div class="rc_layout__md__3">
+                    <div class="overview_feature">
+                        <img src="/assets/images/layout/overview-1.svg">
+                        <p>Reporting and analytics</p>
+                    </div>
+                </div>
+                <div class="rc_layout__md__3">
+                    <div class="overview_feature">
+                        <img src="/assets/images/layout/overview-1.svg">
+                        <p>Trigger-based emails</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="rc_section__container">
+    <div class="rc_section rc_section--integrations">
+        <div class="rc_layout__container">
+            <div class="rc_layout">
+                <div class="rc_layout__md__6 rc_media__container">
+                    <img src="/assets/images/developers/hubble-grow-your-business.png" alt="Huble Website Shot" class="img--responsive">
+                </div>
+                <div class="rc_layout__md__6 rc_content__container">
+                    <div class="rc_content">
+                        <h3>Grow your business with the ReCharge API</h3>
+                        <p>Within the first three months of using ReCharge and its powerful API, Hubble gained more than 17,000 customers and started to process more than 600 orders daily.</p>
+                        <p><button class="rc_button rc_button_primary">Read the case study</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="rc_section__container">
+    <div class="rc_section rc_section--overview rc_text--center">
+        <h3>Are you a developer?</h3>
+        <p>ReCharge provides RESTful API, with robust documentation and full developer support from the ReCharge team.</p>
+        <div class="rc_layout__container">
+            <div class="rc_layout">
+                <div class="rc_layout__md__4">
+                    <div class="overview_feature">
+                        <img src="/assets/images/layout/overview-2.svg">
+                        <h4>Customize for clients</h4>
+                        <p>Break the boundles of the ReCharge app and fully customize how subscriptions work for your clients</p>
+                    </div>
+                </div>
+                <div class="rc_layout__md__4">
+                    <div class="overview_feature">
+                        <img src="/assets/images/layout/overview-2.svg">
+                        <h4>Build your own app</h4>
+                        <p>Now you can build off the ReCharge platform and create your own solutions</p>
+                    </div>
+                </div>
+                <div class="rc_layout__md__4">
+                    <div class="overview_feature">
+                        <img src="/assets/images/layout/overview-2.svg">
+                        <h4>Become a partner</h4>
+                        <p>Gain access to our API documentation with a ReCharge Pro account</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include('templates/static/footer.php'); ?>
