efc14602448bf826d3ec7aecabb8ad6b0eebe5ac
Diego <diegobechi@gmail.com>
2017-12-14 17:07:26 +1100

    added the pricing page

---
+++ homepage/pricing.php
@@ -0,0 +1,138 @@
+<?php
+$page_title = 'Pricing';
+$page_description = 'ReCharge is the best ecommerce subscription solution. It includes everything you need to run a subscription business online. Try free for 90 days.';
+$page_keywords = 'Pricing, Cost, Rate, Subscription Fee, Charge, Buy';
+$page_class = 'pricing';
+$page_active = 'pricing';
+?>
+<?php include('templates/static/header.php'); ?>
+
+
+<div class="rc_section__container rc_pricing__hero">
+	<div class="rc_section rc_section--hero rc_text--center">
+		<div class="rc_content">
+			<h1><span>Priced to fit</span> your subscription business</h1>
+			<p>Our Standard and Pro plans give you access to the recurring payments solution you can depend on.</p>
+		</div>
+	</div>
+</div>
+
+<div class="rc_section__container">
+	<div class="rc_section rc_section--pricing_plans">
+		<div class="rc_layout__container">
+			<div class="rc_layout rc_pricing__plan__container rc_text--center">
+				<div class="rc_layout__md__6">
+					<div class="rc_pricing__plan rc_pricing__plan--regular rc_shadow_level5">
+						<div class="rc_pricing__plan_header" >
+							<span>Standard</span>
+							<div>
+								<p>Up to $100,000 in MRR</p>
+							</div>
+						</div>
+						<div class="rc_pricing__plan_body">
+							<p>$19,99 per month +</p>
+							<h4>1% per recurring order</h4>
+							<div>
+								<ul class="rc_text--left">
+									<li>Full set of ReCharge features</li>
+									<li>Top-notch customer support</li>
+									<li>Integration with the best-of-breed apps</li>
+								</ul>
+							</div>
+							<button class="rc_button rc_button_large rc_button_primary">Get started</button>
+						</div>
+					</div>
+				</div>
+				<div class="rc_layout__md__6">
+					<div class="rc_pricing__plan rc_pricing__plan--pro rc_shadow_level5">
+						<div class="rc_pricing__plan_header" >
+							<span>ReCharge Pro</span>
+							<div>
+								<p>Over $100,000 in MRR</p>
+							</div>
+						</div>
+						<div class="rc_pricing__plan_body">
+							<p>Pricing built to scale</p>
+							<h4>Contact our team</h4>
+							<div>
+								<ul class="rc_text--left">
+									<li>Priority account manager</li>
+									<li>Dedicated checkout servers</li>
+									<li>Reduced processing fee</li>
+								</ul>
+							</div>
+							<button class="rc_button rc_button_large rc_button_secondary">Apply for Pro</button>
+						</div>
+					</div>		
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="rc_section rc_pricing_faqs">
+		<h2>Frequently asked questions</h2>
+		<div id="rc_pricing_faqs_list" role="tablist">
+			<div class="card rc_shadow_level1">
+				<div class="card-header" role="tab" id="headingOne">
+					<h5>
+						How does pricing work?
+						<a class="collapsed" data-toggle="collapse" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+							<span></span>
+						</a>
+					</h5>
+				</div>
+				<div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#rc_pricing_faqs_list">
+					<div class="card-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					</div>
+				</div>
+			</div>
+			<div class="card rc_shadow_level1">
+				<div class="card-header" role="tab" id="headingTwo">
+					<h5>
+						How do I qualify for ReCharge Pro?
+						<a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+							<span></span>
+						</a>
+					</h5>
+				</div>
+				<div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#rc_pricing_faqs_list">
+					<div class="card-body">
+						Stores that have $100K in MRR or higher qualify for ReCharge Pro. Contact us here to get reduced pricing.
+					</div>
+				</div>
+			</div>
+			<div class="card rc_shadow_level1">
+				<div class="card-header" role="tab" id="headingThree">
+					<h5>
+						What is the discount for ReCharge Pro?
+						<a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+							<span></span>
+						</a>
+					</h5>
+				</div>
+				<div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#rc_pricing_faqs_list">
+					<div class="card-body">
+						Pro stores receive a 30% discount. Instead of the standard 1% fee, the processing fee is reduced to 0.7%.
+					</div>
+				</div>
+			</div>
+			<div class="card rc_shadow_level1">
+				<div class="card-header" role="tab" id="headingThree">
+					<h5>
+						What is the discount for Shopify Plus stores?
+						<a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+							<span></span>
+						</a>
+					</h5>
+				</div>
+				<div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#rc_pricing_faqs_list">
+					<div class="card-body">
+						Shopify Plus stores automatically qualify for the Pro discount pricing regardless of your current MRR.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include('templates/static/footer.php'); ?>
